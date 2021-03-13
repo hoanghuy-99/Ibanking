86879a0d834877685d4f566429e1f128adc84e9a
tuankiet15021999 <tuankiet15021999@users.noreply.github.com>
2021-03-13 20:04:03 +0700

    blabla

--- client/src/pages/OTP/index.js
+++ client/src/pages/OTP/index.js
@@ -1,14 +1,28 @@
 import React, {useState} from 'react';
+import OTPInput, { ResendOTP } from "otp-input-react";
 const OTP = (props) =>{
+    const [otp, setOTP] = useState("")  
     return(
         <div>
-           <div class="container my-3">
-                <div class="row justify-content-center">
-                    <div class="col-xl-5 border mt-3 pt-3 rounded mx-3">
-                        <div class="form-group" style={{textAlign:"center"}}>
-                            <button class="btn btn-dark mr-2" with>Quay lại</button>
-                            <button class="btn btn-danger mr-2">Gửi lại</button>
-                            <button class="btn btn-danger mr-2">Xác nhận </button>
+           <div className="container my-3">
+                <div className="row justify-content-center">
+                    <div className="col-xl-5 border mt-3 pt-3 rounded mx-3">
+                        <div className="form-group" style={{textAlign:"center", justifyContent:"center"}}>
+                            <h4>Xác nhận OTP</h4>
+                            <div>
+                            <OTPInput
+                                value={otp}
+                                onChange={setOTP}
+                                autoFocus
+                                OTPLength={6}
+                                otpType="number"
+                                disabled={false}
+                                secure
+                            />
+                            </div>
+                            <button className="btn btn-dark m-1" with>Quay lại</button>
+                            <button className="btn btn-danger m-1">Gửi lại</button>
+                            <button className="btn btn-danger m-1">Xác nhận </button>
                         </div>
                     </div>
                 </div>
--- client/src/pages/Transaction/index.js
+++ client/src/pages/Transaction/index.js
@@ -2,58 +2,58 @@ import React, {useState} from 'react';
 const Transaction = (props) =>{
     return(
         <div>
-            <div class="container my-3">
-                <div class="row justify-content-center">
-                    <div class="col-xl-8 border mt-3 pt-3 rounded mx-3">
+            <div className="container my-3">
+                <div className="row justify-content-center">
+                    <div className="col-xl-8 border mt-3 pt-3 rounded mx-3">
                         <form action="" method="POST">
                             <div>
                                 <h4>Người nộp tiền</h4>
-                                <div class="form-group">
+                                <div className="form-group">
                                     <label for="user">Họ tên</label>
-                                    <input class="form-control" type="text"  id="name" name="name" disabled/>
+                                    <input className="form-control" type="text"  id="name" name="name" disabled/>
                                 </div>
-                                <div class="form-group">
+                                <div className="form-group">
                                     <label for="std">Số điện thoại</label>
-                                    <input class="form-control" type="text" id="sdt" name="sdt" disabled/>
+                                    <input className="form-control" type="text" id="sdt" name="sdt" disabled/>
                                 </div>
-                                <div class="form-group">
+                                <div className="form-group">
                                     <label for="email">Email</label>
-                                    <input class="form-control" type="text" id="email" name="email" disabled/>
+                                    <input className="form-control" type="text" id="email" name="email" disabled/>
                                 </div>
                             </div>
-                            <div class="my-3">
+                            <div className="my-3">
                                 <h4>Thông tin học phí</h4>
-                                <div class="form-group">
+                                <div className="form-group">
                                     <label for="mssv">Mã số sinh viên</label>
-                                    <input class="form-control" type="text"  placeholder="Nhập MSSV" id="mssv" name="mssv"/>
+                                    <input className="form-control" type="text"  placeholder="Nhập MSSV" id="mssv" name="mssv"/>
                                 </div>
-                                <div class="form-group">
+                                <div className="form-group">
                                     <label for="hoten">Họ và tên sinh viên</label>
-                                    <input class="form-control" type="text"  id="name_sv" name="name_sv" disabled/>
+                                    <input className="form-control" type="text"  id="name_sv" name="name_sv" disabled/>
                                 </div>
-                                <div class="form-group">
+                                <div className="form-group">
                                     <label for="user">Số tiền cần nộp</label>
-                                    <input class="form-control" type="text" id="money" name="money" value="VND" disabled/>
+                                    <input className="form-control" type="text" id="money" name="money" value="VND" disabled/>
                                 </div>
                             </div>
-                            <div class="my-3">
+                            <div className="my-3">
                                 <h4>Thông tin thanh toán</h4>
-                                <div class="form-group">
+                                <div className="form-group">
                                     <label for="sodu">Số dư tài khỏan</label>
-                                    <input class="form-control" type="text"  id="sodu" name="sodu" disabled/>
+                                    <input className="form-control" type="text"  id="sodu" name="sodu" disabled/>
                                 </div>
-                                <div class="form-group">
+                                <div className="form-group">
                                     <label for="tiennop">Số tiền cần nộp</label>
-                                    <input class="form-control" type="text"  id="tiennop" name="tiennop" disabled/>
+                                    <input className="form-control" type="text"  id="tiennop" name="tiennop" disabled/>
                                 </div>
-                                <div class="form-group">
+                                <div className="form-group">
                                     <label for="user">Thỏa thuận&Điều khoản</label>
-                                    <textarea class="form-control" name="desciption" id="desciption" cols="30" rows="10" disabled></textarea>
+                                    <textarea className="form-control" name="desciption" id="desciption" cols="30" rows="10" disabled></textarea>
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <button class="btn btn-dark mr-2" with>Hủy</button>
-                                <button class="btn btn-danger mr-2">Thanh toán</button>
+                            <div className="form-group">
+                                <button className="btn btn-dark m-1" with>Hủy</button>
+                                <button className="btn btn-danger m-1">Thanh toán</button>
                             </div>
                         </form>
                     </div>
