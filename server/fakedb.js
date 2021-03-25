61a68f46e91402ef4d9f29491e0f6c717d869e03
tuankiet15021999 <tuankiet15021999@users.noreply.github.com>
2021-03-25 18:14:08 +0700

    db

--- server/fakedb.js
+++ server/fakedb.js
@@ -19,10 +19,19 @@ async function fakeData()
 
     const user = new User({
         _id: new mongoose.Types.ObjectId(),
-        username:'Kiet',
+        username:'tuankiet1502',
         hashedPassword: hashed_password,
         name:'Kiet',
         emailAddress:'tuankiet15021999@gmail.com',
+        phone:'0909123456',
+        balance: 1000000000000
+    })
+    const user2 = new User({
+        _id: new mongoose.Types.ObjectId(),
+        username:'hoanghuy',
+        hashedPassword: hashed_password,
+        name:'Vũ Hoàng Huy',
+        emailAddress:'vuhoanghuy29101999@gmail.com',
         phone:'0909123456',
         balance: 1000000000000
     })
@@ -35,21 +44,29 @@ async function fakeData()
         description: "Học phí Học kì 2 năm học 2020-2021",
         amount: 2000000
     })
-
-    const tran = new Transaction({
+    const debt2 = new Debt({
         _id: new mongoose.Types.ObjectId(),
-        debt: debt._id,
-        user: user._id
+        student:{
+            id: "51702036",
+            name:"Dương Hữu Nguyên"
+        },
+        description: "Tiền bảo hiểm y tế năm 2020-2021",
+        amount: 50000000
     })
-    user.transactions.push(tran._id)
-    const docs = [user, debt, tran]
+    const debt3 = new Debt({
+        _id: new mongoose.Types.ObjectId(),
+        student:{
+            id: "51702117",
+            name:"Vũ Hoàng Huy"
+        },
+        description: "Tiền rớt môn",
+        amount: 100000000
+    })
+    const docs = [user,user2,debt,debt2,debt3]
     for(let i = 0; i < docs.length; i++ )
     {   
         await docs[i].save()
     }
-    const trans = await Transaction.find({user:user._id}).populate('debt')
-    
-    console.log(trans)
 }
 fakeData().then(()=>{
     console.log('Fake data were created')
